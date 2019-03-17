344252b02dd5f8a5f9f86c730443dd55cc067ccf
Richie <rgoulazian@gmail.com>
2019-03-17 13:09:09 -0400

    Fixed cloning error for sequential models

--- AI/SequentialModel.cs
+++ AI/SequentialModel.cs
@@ -9,9 +9,24 @@ namespace ModularGenetics.AI
     public class SequentialModel : PhenotypeGroup
     {
         private ComputationModel[] computationModels;
+        public ComputationModel[] ComputationModels => computationModels;
         public SequentialModel(ComputationModel[] computationModels) : base(computationModels)
         {
             this.computationModels = computationModels;
+        }
+
+        public override Phenotype Clone(Phenotype otherParent)
+        {
+            //Assume to be another phenotype group
+            SequentialModel modelParent = (SequentialModel)otherParent;
+
+            ComputationModel[] modelClones = new ComputationModel[computationModels.Length];
+            for (int i = 0; i < modelClones.Length; i++)
+            {
+                modelClones[i] = (ComputationModel)computationModels[i].Clone(modelParent.ComputationModels[i]);
+            }
+
+            return new SequentialModel(modelClones);
         }
 
         public object Transform(object input)
